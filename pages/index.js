1e2226839260f725de8e13987ba9161c9ece77b7
LewWilde <lew.wilde@outlook.com>
2021-11-05 17:21:51 +0000

    next.js and workflows

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Lew Wilde</title>
+        <meta name="description" content="Lew Wilde" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Waiting to be born
+        </h1>
+      </main>
+
+      <footer className={styles.footer}>
+        lewwilde.co.uk
+      </footer>
+    </div >
+  )
+}
