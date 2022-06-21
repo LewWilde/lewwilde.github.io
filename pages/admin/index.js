86dd0442f32a101fda84b02937f8f445f2179940
LewWilde <lew.wilde@outlook.com>
2022-06-21 23:27:48 +0100

    basic netlify cms setup

---
+++ pages/admin/index.js
@@ -0,0 +1,57 @@
+import dynamic from 'next/dynamic'
+
+const config = {
+
+    cms_manual_init: true,
+    load_config_file: false,
+    local_backend: true,
+    backend: {
+        name: 'github',
+        repo: 'LewWilde/lewwilde.github.io',
+        branch: 'main',
+    },
+    publish_mode: 'editorial_workflow',
+    media_folder: '/public/img',
+    collections: [
+        {
+            name: "Posts",
+            label: "Posts",
+            editor: { preview: false },
+            label_singular: "Post",
+            folder: "content/posts",
+            create: true,
+            slug: "{{slug}}",
+            extension: "md",
+            format: "yaml-frontmatter",
+            fields: [
+                {
+                    label: "Title",
+                    name: "title",
+                    widget: "string",
+                    required: true
+                },
+                { label: "Publish Date", name: "date", widget: "datetime" },
+                {
+                    label: "Body", name: "body", widget: "markdown"
+                }
+            ],
+        },
+    ],
+
+}
+
+const NetlifyCMS = dynamic(
+    () => {
+        return import('netlify-cms-app').then((CMS) => {
+            CMS.init({ config })
+        })
+    },
+    { ssr: false }
+
+)
+
+const Admin = () => <>
+    <NetlifyCMS />
+</>
+
+export default Admin
