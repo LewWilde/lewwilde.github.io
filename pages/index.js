088e555d7a576500de3506b54eb5ae3ecaf21d87
LewWilde <lew.wilde@outlook.com>
2025-01-13 20:50:04 +0000

    Update dependencies

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,25 @@
 import Head from 'next/head'
-import NavBar from '../components/NavBar'
 import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 import WelcomeTime from '../components/welcomeTime'
-import MiniMap from '../components/MiniMap/MiniMap'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Lew Wilde</title>
-        <meta name="description" content="Lew Wilde" />
+    return (
+        <>
+            <Head>
+                <title>Lew Wilde</title>
+                <meta name="description" content="Lew Wilde" />
 
-      </Head>
+            </Head>
 
-      <NavBar hello={'hello'}></NavBar>
-      <div className={styles.container}>
+            <div className={styles.container}>
 
+                <main className={styles.main}>
+                    <WelcomeTime />
+                    <p>{"I'm a web designer from the wonderful town of Blackburn. I work for Billian I.T Solutions, creating travel websites under the brand"}
+                        <a href="https://designfortravel.co.uk">Design for Travel</a></p>
+                </main>
 
-        <main className={styles.main}>
-          <WelcomeTime />
-          <p>{"I'm a web designer from the wonderful town of Blackburn. I work for Billian I.T Solutions, creating travel websites under the brand"}
-            <a href="https://designfortravel.co.uk">Design for Travel.</a></p>
-        </main>
-
-        <MiniMap />
-
-      </div >
-    </>
-  )
+            </div >
+        </>
+    )
 }
