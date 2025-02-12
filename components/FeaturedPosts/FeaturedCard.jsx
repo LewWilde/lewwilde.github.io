ef8fe30d56d2465016cac8e48d1e18fe9a0507c8
Lew Wilde <lew.wilde@outlook.com>
2025-02-12 23:27:34 +0000

    feat: add featured card decoration

--- components/FeaturedPosts/FeaturedCard.jsx
+++ components/FeaturedPosts/FeaturedCard.jsx
@@ -1,19 +1,22 @@
 import { Image } from "../Image/Image"
 import NextLink from "next/link";
 import css from './FeaturedCard.module.scss'
+import { FolderOpen } from "@phosphor-icons/react/dist/ssr";
+import { ArrowFatLinesRight } from "@phosphor-icons/react/dist/ssr";
+import { Button } from "../Buttons/Button";
 
-export const FeaturedCard = ({ featuredimage, name, slug }) => {
+export const FeaturedCard = ({ featuredimage, name, slug, path }) => {
 
     return (<NextLink className={css.card} href={slug}>
         <div className={css['card_image-wrap']} >
             <Image className={css.card_image} maxWidth={1100} {...featuredimage} alt={name} />
         </div>
-
         <div className={css.card_content}>
             <div className={css['card_content-wrap']}>
+                <div className={css['card_category']}><FolderOpen weight="bold" className={css['card_category_icon']} />/ {path}</div>
                 {name}
             </div>
-
+            <Button variant={"inverted"} element={"div"}><ArrowFatLinesRight weight="bold" size={"1em"} /></Button>
         </div>
     </NextLink >)
 }
--- components/FeaturedPosts/FeaturedPosts.jsx
+++ components/FeaturedPosts/FeaturedPosts.jsx
@@ -11,14 +11,14 @@ export const FeaturedPosts = ({ heading, text, buttonText, buttonHref, documents
             <div className={css.intro}>
                 <div className={css.intro_wrap}>
                     <h2 className={css.intro_heading}>{heading}</h2>
-                    <div lassName={css.intro_text}>{text}</div>
+                    <div className={css.intro_text}>{text}</div>
                     {buttonHref &&
                         <div className={css.intro_cta}>
                             <Button href={buttonHref} element={NextLink}>{buttonText ?? 'View All'}</Button>
                         </div>}
                 </div>
             </div>
-            {documents?.map(document => <FeaturedCard key={document.slug} {...document} />)}
+            {documents?.map(document => <FeaturedCard key={document.slug} path={'projects'} {...document} />)}
         </section>
 
     )
