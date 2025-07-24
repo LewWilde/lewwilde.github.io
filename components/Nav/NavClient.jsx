f9af43143beb605aa88df240875d89e635d12d34
Lew Wilde <lew.wilde@outlook.com>
2025-07-24 23:01:31 +0100

    feat: nav link styling

--- components/Nav/NavClient.jsx
+++ components/Nav/NavClient.jsx
@@ -4,7 +4,6 @@ import * as Dialog from "@radix-ui/react-dialog"
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden"
 import css from './Nav.module.scss'
 import { Button } from "@/components/Buttons/Button"
-import { Container } from "../Layout/Container"
 import { useState } from "react";
 import { NavLink } from "@/components/Nav/NavLink";
 import { resolveLink } from "../../utils/resolveLink";
@@ -31,26 +30,25 @@ export const NavClient = ({ items }) => {
                             >Close
                             </Button>} />
                         </div>
-                        <Container>
-                            <div className={css.grid}>
-                                <div className={css.grid_left}
-                                ></div>
-                                <div className={css.grid_right}>
-                                    <nav className={css.nav}>
-                                        {items.map(item =>
-                                            <NavLink key={item._key}
-                                                href={item.internal ? resolveLink(item.internal) : item.external}
-                                                isExternal={!!item.external}
-                                                onNavigate={() => { setOpen(false) }}
-                                            >{item.text}</NavLink>
-                                        )}
-                                    </nav>
-                                </div>
-                                <div className={css.grid_footer} >
 
-                                </div>
+                        <div className={css.grid}>
+                            <div className={css.grid_left}
+                            ></div>
+                            <div className={css.grid_right}>
+                                <nav className={css.nav}>
+                                    {items.map(item =>
+                                        <NavLink key={item._key}
+                                            href={item.internal ? resolveLink(item.internal) : item.external}
+                                            isExternal={!!item.external}
+                                            onNavigate={() => { setOpen(false) }}
+                                        >{item.text}</NavLink>
+                                    )}
+                                </nav>
                             </div>
-                        </Container>
+                            <div className={css.grid_footer} >
+                            </div>
+                        </div>
+
                     </Dialog.Content>
                 </Dialog.Overlay>
             </Dialog.Portal>
