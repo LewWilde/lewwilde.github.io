a7bf16b9ef9d96f8387d018fdc05165b6a5bcc48
Lew Wilde <lew.wilde@outlook.com>
2025-07-28 21:20:31 +0100

    feat: add page content feed to project archive

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -7,36 +7,47 @@ import { Card } from "@/components/Card/Card";
 import { Pagination } from "@/components/Pagination/Pagination"
 import { CardsGrid } from "@/components/Card/CardsGrid";
 
+import { portableTextComponents } from '@/components/PortableText/portableTextComponents';
+import { PortableText } from '@portabletext/react';
+
 import css from './page.module.scss';
 
 const postsPerPage = 18;
-const postCountGROQ = `count(* [_type == "project"])`;
 
 export default async function Page({ searchParams }) {
 
-    const page = parseInt((await searchParams)?.page ?? 1);
+    const pageNumber = parseInt((await searchParams)?.page ?? 1);
 
-    const GROQ = `*[_type == "project"] | order(_id) [${(postsPerPage * (page - 1))}...${(postsPerPage * (page - 1)) + postsPerPage}] {
+    const pageQROQ = `*[_type == "page" && slug.current == "projects"][0]`
+    const postsGROQ = `*[_type == "project"] | order(_id) [${(postsPerPage * (pageNumber - 1))}...${(postsPerPage * (pageNumber - 1)) + postsPerPage}] {
       _id, 
       title, 
       featuredimage,
       "slug": '/projects/' + slug.current,
     }`
+    const postCountGROQ = `count(* [_type == "project"])`;
 
-    const posts = await client.fetch(GROQ) || []
-    const postCount = await client.fetch(postCountGROQ) || 0;
+    const GROQ = `{
+        "page" : ${pageQROQ},
+        "posts" : ${postsGROQ},
+        "postCount" : ${postCountGROQ},
+    }`
+
+    const data = await client.fetch(GROQ) || {};
+
+    const { page, posts, postCount } = data;
     const pageCount = Math.ceil(postCount / postsPerPage);
-    const isFirstPage = page <= 1;
-    const isLastPage = page === pageCount;
+    const isFirstPage = pageNumber <= 1;
+    const isLastPage = pageNumber === pageCount;
 
     return (
         <Container>
             <div className={css.page_content}>
                 <div className={css.page_header}>
                     <Heading level='1'>
-                        Projects
+                        {page?.title}
                     </Heading>
-
+                    {page?.body && <PortableText value={page.body} components={portableTextComponents} />}
                 </div>
                 {posts?.length > 0 &&
                     <CardsGrid>
@@ -47,9 +58,9 @@ export default async function Page({ searchParams }) {
                 {pageCount > 0 &&
                     <div className={css.page_pagination}>
                         <Pagination>
-                            <Pagination.Prev disabled={isFirstPage} element={!isFirstPage ? Link : 'button'} href={!isFirstPage ? `/projects?page=${page - 1}` : undefined} />
-                            <Pagination.PageNumbers page={page} pageCount={pageCount} />
-                            <Pagination.Next disabled={isLastPage} element={!isLastPage ? Link : 'button'} href={!isLastPage ? `/projects?page=${page + 1}` : undefined} />
+                            <Pagination.Prev disabled={isFirstPage} element={!isFirstPage ? Link : 'button'} href={!isFirstPage ? `/projects?page=${pageNumber - 1}` : undefined} />
+                            <Pagination.PageNumbers page={pageNumber} pageCount={pageCount} />
+                            <Pagination.Next disabled={isLastPage} element={!isLastPage ? Link : 'button'} href={!isLastPage ? `/projects?page=${pageNumber + 1}` : undefined} />
                         </Pagination>
                     </div>
                 }
