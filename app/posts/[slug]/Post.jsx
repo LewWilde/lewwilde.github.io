e9c97561c40a608fcc387e54ab80700e8e562d2c
Lew Wilde <lew.wilde@outlook.com>
2026-01-19 22:36:18 +0000

    feat: add provsional post layout

--- app/posts/[slug]/Post.jsx
+++ app/posts/[slug]/Post.jsx
@@ -1,5 +1,13 @@
 import { client } from '../../../sanityclient';
 
+import { Container } from '@/components/Layout/Container';
+import { PortableText } from '@portabletext/react';
+import { portableTextComponents } from '@/components/PortableText/portableTextComponents';
+import { Article } from '@/components/Layout/Article';
+import { Heading } from '@/components/Typography/Heading';
+import { Breadcrumbs, Crumb } from '@/components/Breadcrumbs/Breadcrumbs';
+import { resolvePath } from '../../../utils/resolvePath';
+
 export async function generateStaticParams() {
 
     const posts = await client.fetch(
@@ -10,20 +18,41 @@ export async function generateStaticParams() {
 
 }
 
+const GROQ = `*[_type == "post" && slug.current == $slug][0] {
+    ...,
+    body[] {
+            ...,
+            _type == "image" => {
+                ...,
+                "alt": asset->altText,
+
+            },
+        },
+  }`;
+
 export default async function Post({ params }) {
 
     const { slug } = await params;
 
-    const post = await client.fetch(`*[_type == "post" && slug.current == $slug][0]`, { slug }) ?? {}
+    const post = await client.fetch(GROQ, { slug }) ?? {}
 
-    const { title, _updatedAt } = post;
-
+    const { title, body, _updatedAt, _type } = post;
+    const path = resolvePath({ _type });
+    console.log(post);
 
     return (
-        <div>
-            {title}
-            {_updatedAt}
-        </div>
+        <Container>
+            <Article position="center">
+                <Article.Content>
+                    <Breadcrumbs>
+                        <Crumb href={`/${path}`}>{path}</Crumb>
+                    </Breadcrumbs>
+                    <Heading level={1}>{title}</Heading>
+                    <div>{_updatedAt}</div>
+                    <PortableText value={body} components={portableTextComponents} />
+                </Article.Content>
+            </Article>
+        </Container>
     )
 
 }
