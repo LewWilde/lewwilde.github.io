caa7dfa293207d2a5eb1ad47f1a4d31410fdab84
Lew Wilde <lew.wilde@outlook.com>
2026-01-22 21:39:07 +0000

    feat: revised footer design with colours

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,5 +1,4 @@
 import css from './Footer.module.scss';
-import { Container } from '../Layout/Container';
 
 export const Footer = () => {
 
@@ -12,11 +11,36 @@ export const Footer = () => {
         return hash;
     }
 
+    const gradientDark = ['grey-1200', 'grey-1100', 'grey-1000', 'grey-900', 'grey-800'];
+    const gradientLight = ['grey-700', 'grey-600', 'grey-500', 'grey-400', 'grey-300'];
+    const gradientAccent = ['yellow-500', 'yellow-400', 'yellow-300', 'yellow-200', 'yellow-100'];
+
+    console.log(gradientDark, gradientLight, gradientAccent)
+
     return (
         <div className={css.footer}>
-            <Container>
-                <div className={css.footer_build}>Build:{getHash()}</div>
-            </Container>
+            <div className={css.hash}></div>
+            <div className={css.swatch_row}>
+                <div className={css.swatch_col}>
+                    <div className={css.swatch}>
+                        {gradientDark.map(c => <div key={c} className={`${css.swatch_cell} ${css['swatch_cell--bg-' + c]}`}></div>)}
+                    </div>
+                    <div className={css.swatch}>
+                        {gradientLight.map(c => <div key={c} className={`${css.swatch_cell} ${css['swatch_cell--bg-' + c]}`}></div>)}
+                    </div>
+                </div>
+                <div className={css.swatch_col}>
+                    <div className={css.swatch}>
+                        {gradientAccent.map(c => <div key={c} className={`${css.swatch_cell} ${css['swatch_cell--bg-' + c]}`}></div>)}
+                    </div>
+                    <div className={css.swatch}>
+
+                        <div className={`${css.swatch_cell} ${css['swatch_cell--bg-grey-1200']} ${css['swatch_cell--wide']}`}>
+                            <div className={css.copyright} >© - Just credit me. Build:{getHash()}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
