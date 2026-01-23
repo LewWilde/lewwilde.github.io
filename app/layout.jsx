cc30c5759c89a41390c10b47eb926d2547921bbd
LewWilde <lew.wilde@outlook.com>
2026-01-23 19:28:58 +0000

    refactor: use next/font for fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,15 +1,18 @@
 import '../styles/globals.scss'
 import { Header } from '@/components/Header/Header';
 import { Footer } from '@/components/Footer/Footer';
+import { Saira } from 'next/font/google'
+
+const saira = Saira({
+    subsets: ['latin'],
+    display: 'swap',
+})
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en">
+        <html lang="en" className={saira.className}>
             <head>
                 <link rel="icon" href="/favicon.ico" />
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
-                <link href="https://fonts.googleapis.com/css2?family=Saira:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
             </head>
             <body>
                 <Header />
