c1f20d4abd80b0644fbc784d745e0eba998d7f31
LewWilde <lew.wilde@outlook.com>
2026-01-23 19:20:21 +0000

    refactor: move scroll handling to fix dependency warning

--- components/Layout/StickyAside.jsx
+++ components/Layout/StickyAside.jsx
@@ -11,40 +11,39 @@ export const StickyAside = ({ children }) => {
 
     let lastScrollTop = typeof document == "undefined" ? 0 : document.body.scrollTop;
 
-    const handleScroll = () => {
-        const wrapper = wrapperRef.current;
-        const margin = marginRef.current;
-        const container = containerRef.current;
-
-        if (!(wrapper && margin && container)) { return; }
-
-        const { height } = container.getBoundingClientRect()
-        const { scrollTop } = document.documentElement;
-        const direction = scrollTop > lastScrollTop ? 1 : 0;
-
-        lastScrollTop = scrollTop;
-        if (height <= window.innerHeight) {
-            container.style.top = "0px",
-                container.style.bottom = "",
-                margin.style.marginTop = "";
-            return
-        }
-
-        const wrapperRect = wrapper.getBoundingClientRect()
-        const containerRect = container.getBoundingClientRect();
-        margin.style.marginTop = `${containerRect.top - wrapperRect.top}px`;
-
-        direction === 1 ? (
-            container.style.bottom = "",
-            container.style.top = `-${height - window.innerHeight}px`
-        ) : (
-            container.style.top = "",
-            container.style.bottom = `-${height - window.innerHeight}px`
-        )
-
-    }
-
     useEffect(() => {
+        const handleScroll = () => {
+            const wrapper = wrapperRef.current;
+            const margin = marginRef.current;
+            const container = containerRef.current;
+
+            if (!(wrapper && margin && container)) { return; }
+
+            const { height } = container.getBoundingClientRect()
+            const { scrollTop } = document.documentElement;
+            const direction = scrollTop > lastScrollTop ? 1 : 0;
+
+            lastScrollTop = scrollTop;
+            if (height <= window.innerHeight) {
+                container.style.top = "0px",
+                    container.style.bottom = "",
+                    margin.style.marginTop = "";
+                return
+            }
+
+            const wrapperRect = wrapper.getBoundingClientRect()
+            const containerRect = container.getBoundingClientRect();
+            margin.style.marginTop = `${containerRect.top - wrapperRect.top}px`;
+
+            direction === 1 ? (
+                container.style.bottom = "",
+                container.style.top = `-${height - window.innerHeight}px`
+            ) : (
+                container.style.top = "",
+                container.style.bottom = `-${height - window.innerHeight}px`
+            )
+
+        }
 
         window.removeEventListener('scroll', handleScroll);
         window.addEventListener('scroll', handleScroll, { passive: true });
