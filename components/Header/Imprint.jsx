d3f2ce693a28fcd6b80bb7649c989a4a0cd4bf37
LewWilde <lew.wilde@outlook.com>
2026-01-23 19:02:29 +0000

    refactor: swap to nextLink to remove warning

--- components/Header/Imprint.jsx
+++ components/Header/Imprint.jsx
@@ -1,16 +1,16 @@
-import Link from 'next/link'
+import NextLink from 'next/link'
 import css from './Imprint.module.scss';
 
 export const Imprint = () => {
 
     return (
-        <Link href="/" className={css.imprint}>
+        <NextLink href="/" className={css.imprint}>
             <div className={`${css.mark} ${css['mark--tl']}`}></div>
             <div className={`${css.mark} ${css['mark--tr']}`}></div>
             <div className={`${css.mark} ${css['mark--br']}`}></div>
             <div className={`${css.mark} ${css['mark--bl']}`}></div>
             <span>Lew<span className={css.separator} aria-hidden="true">_</span>Wilde</span>
-        </Link>
+        </NextLink>
     )
 
 }
